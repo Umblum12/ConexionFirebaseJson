be8cb4f7356df9064c1abd53efe469e13b8192cd
Umblum12 <68798247+Umblum12@users.noreply.github.com>
2023-03-19 02:15:19 -0600

    ActualizacionBotonMostrarHistorial

--- ConexionFirebaseJson/ConexionFirebaseJson/MainPage.xaml.cs
+++ ConexionFirebaseJson/ConexionFirebaseJson/MainPage.xaml.cs
@@ -8,6 +8,12 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Newtonsoft.Json;
 using System.Net;
+using System.Collections.ObjectModel;
+using System.IO;
+using static ConexionFirebaseJson.MainPage;
+using Newtonsoft.Json.Linq;
+using System.Diagnostics;
+using System.Xml.Linq;
 
 namespace ConexionFirebaseJson
 {
@@ -17,36 +23,47 @@ namespace ConexionFirebaseJson
         {
             InitializeComponent();
         }
-
-        public class Historial
+        public class ConsumoAgua
         {
-            public string Nombre { get; set; }
-            public DateTime? Fecha { get; set; }
-            public int Flujo { get; set; }
-            public bool Estado { get; set; }
+            public Dictionary<string, MedidaAgua> Historial { get; set; }
         }
+
+        public class MedidaAgua
+        {
+            public bool Estado { get; set; }
+            public DateTime Fecha { get; set; }
+            public int Flujo { get; set; }
+            public string Nombre { get; set; }
+        }
+
+        public async Task<ConsumoAgua> GetConsumoAgua()
+        {
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync("https://consumodeaguapi-default-rtdb.firebaseio.com/Consumodeagua.json");
+            var json = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<ConsumoAgua>(json);
+        }
+
+
 
         private async void Button_Clicked(object sneder,EventArgs e)
         {
-            var request = new HttpRequestMessage();
-            request.RequestUri = new Uri("https://consumodeaguapi-default-rtdb.firebaseio.com/Historial.json");
-            request.Method = HttpMethod.Get;
-            request.Headers.Add("Accept", "application/json");
-            var client = new HttpClient();
-            HttpResponseMessage response = await client.SendAsync(request);
-            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            await Task.Run(async () =>
             {
-                string content = await response.Content.ReadAsStringAsync();
-                var resultado = JsonConvert.DeserializeObject <List<Historial>> (content);
-                listahistorial.ItemsSource = resultado;
-            }
+                var consumoAgua = await GetConsumoAgua();
+                var medidasAgua = new List<MedidaAgua>(consumoAgua.Historial.Values);
+                Device.BeginInvokeOnMainThread(() =>
+                {
+                    miListView.ItemsSource = medidasAgua;
+                });
+            });
         }
         private async void Button_Clicked1(object sneder,EventArgs e)
         {
             DateTime dateValue;
             string dateString = "03/17/2023 01:24:00 PM";
                 dateValue = DateTime.Parse(dateString);
-                Historial his = new Historial
+            MedidaAgua model = new MedidaAgua
             {
                 Nombre = "Abelardo",
                 Fecha = dateValue,
@@ -54,9 +71,9 @@ namespace ConexionFirebaseJson
                 Estado = false
             };
 
-            Uri RequestUri = new Uri("https://consumodeaguapi-default-rtdb.firebaseio.com/Historial.json");
+            Uri RequestUri = new Uri("https://consumodeaguapi-default-rtdb.firebaseio.com/Consumodeagua/Historial.json");
             var client = new HttpClient();
-            var json = JsonConvert.SerializeObject(his);
+            var json = JsonConvert.SerializeObject(model);
             var contentJson = new StringContent(json, Encoding.UTF8, "application/json");
             var response = await client.PostAsync(RequestUri, contentJson);
             if (response.StatusCode == HttpStatusCode.OK)
--- ConexionFirebaseJson/ConexionFirebaseJson/obj/Debug/netstandard2.0/MainPage.xaml.g.cs
+++ ConexionFirebaseJson/ConexionFirebaseJson/obj/Debug/netstandard2.0/MainPage.xaml.g.cs
@@ -17,12 +17,12 @@ namespace ConexionFirebaseJson {
     public partial class MainPage : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
-        private global::Xamarin.Forms.CollectionView listahistorial;
+        private global::Xamarin.Forms.CollectionView miListView;
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(MainPage));
-            listahistorial = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.CollectionView>(this, "listahistorial");
+            miListView = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.CollectionView>(this, "miListView");
         }
     }
 }
